e5bb52af45325e67ccfbce0d5d3f2ca6e7812ef8
Dorian Alvarado <dorianalvarado@MacBook-Pro-de-wolox.local>
2020-04-04 02:29:50 -0300

    Added missing setHeaders method

--- src/app/components/Routes/index.js
+++ src/app/components/Routes/index.js
@@ -3,18 +3,22 @@ import React, { useEffect } from 'react';
 import { useGlobalValue } from '@context';
 import { actionsCreator } from '@context/user/actions';
 import { useRequest } from '@hooks';
-import { getUserToken } from '@services/UserService';
+import { getAuthHeaders } from '@services/UserService';
+import api from '@config/api';
 
 import AppRoutes from './layout';
 
 function RoutesContainer() {
-  const { data, loading } = useRequest(getUserToken);
+  const { data: response, loading } = useRequest(getAuthHeaders);
   const [{ userState }, dispatch] = useGlobalValue();
+  const headers = response?.data;
   const token = userState?.token;
-
   useEffect(() => {
-    if (data && !token) dispatch(actionsCreator.logIn(data));
-  }, [dispatch, data, token]);
+    if (headers) {
+      api.setHeaders(headers);
+      dispatch(actionsCreator.logIn(headers['access-token']));
+    }
+  }, [dispatch, headers]);
 
   return <AppRoutes isLoading={loading} token={token} />;
 }
